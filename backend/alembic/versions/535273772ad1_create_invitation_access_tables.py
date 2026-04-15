f3fd6af617f8cfd5935c570a4fab8f0e7c48c895
Mohamed Gassem <mohamed.gassem@gmail.com>
2026-04-15 09:41:08 +0200

    fix(backend): fix migration import style for plan 4 migration

--- backend/alembic/versions/535273772ad1_create_invitation_access_tables.py
+++ backend/alembic/versions/535273772ad1_create_invitation_access_tables.py
@@ -5,17 +5,16 @@ Revises: b416216cd44d
 Create Date: 2026-04-15 09:37:05.768152
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '535273772ad1'
-down_revision: Union[str, Sequence[str], None] = 'b416216cd44d'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision: str = "535273772ad1"
+down_revision: str | Sequence[str] | None = "b416216cd44d"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
